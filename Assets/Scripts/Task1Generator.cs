79d8fff564575b15f17265b653f961e1902b1e6e
Alec Thomson <apt@apthomson.com>
2019-04-12 15:00:55 -0400

    updated for assignment 6.

--- Assets/Scripts/Task1Generator.cs
+++ Assets/Scripts/Task1Generator.cs
@@ -1,43 +1,36 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.SceneManagement;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class Task1Generator : MonoBehaviour {
 
-    public GameObject nodeDisplayPrefab;
+    public Grammar myGrammar;
 
-    private GameObject _rootObj;
-    private TreeNode _root;
+    public Text displayText;
 
-    public int minDesiredNodes = 12;
-    public int maxDesiredNodes = 16;
-
-    void Start() {
-        _root = SpaceTree.createTree();
-        _rootObj = SpaceTree.spawnTreeDisplay(_root, nodeDisplayPrefab, transform);
-    }
 
     void Update() {
+        string displayString = "Press R to Reset.\n" +
+            "Press Space to expand grammar once\n" +
+            "Press Enter to expand grammar x5\n" +
+            "\nCurrent String:\n\n" +
+            myGrammar.currentString;
+
+        displayText.text = displayString;
+
         if (Input.GetKeyDown(KeyCode.R)) {
             SceneManager.LoadScene(SceneManager.GetActiveScene().name);
         }
-        if (Input.GetKeyDown(KeyCode.Space)) {
-            if (_rootObj != null) {
-                Destroy(_rootObj);
-            }
-            _root.doRandomSplit();
-            _rootObj = SpaceTree.spawnTreeDisplay(_root, nodeDisplayPrefab, transform);
+        else if (Input.GetKeyDown(KeyCode.Space)) {
+            myGrammar.expandGrammar();
         }
-        if (Input.GetKeyDown(KeyCode.Return)) {
-            if (_rootObj != null) {
-                Destroy(_rootObj);
+        else if (Input.GetKeyDown(KeyCode.Return)) {
+            for (int i = 0; i < 5; i++) {
+                myGrammar.expandGrammar();
             }
-            int desiredNodes = Random.Range(minDesiredNodes, maxDesiredNodes + 1);
-            _root = SpaceTree.createTree(desiredNodes);
-            _rootObj = SpaceTree.spawnTreeDisplay(_root, nodeDisplayPrefab, transform);
         }
     }
-   
+
 }
